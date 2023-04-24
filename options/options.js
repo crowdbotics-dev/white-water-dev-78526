68c760a7ba562fce973096df2627794bf7247612
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 00:16:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-water-dev-78526",
+  url: "https://white-water-dev-78526.botics.co",
+  api: "https://white-water-dev-78526.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
